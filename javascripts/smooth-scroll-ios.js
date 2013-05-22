88146ff25313d70e374d0d981bd5d9eb6fa0505b
leibai <blwin2010@163.com>
2013-05-23 01:12:28 +0800

    5.23

---
+++ javascripts/smooth-scroll-ios.js
@@ -0,0 +1,26 @@
+$(function(){
+
+$(‘a[href*=#]‘).click(function() {
+
+if (location.pathname.replace(/^\//,”) == this.pathname.replace(/^\//,”)
+&& location.hostname == this.hostname) {
+
+var $target = $(this.hash);
+
+$target = $target.length && $target || $(‘[name=' + this.hash.slice(1) +']‘);
+
+if ($target.length) {
+
+var targetOffset = $target.offset().top;
+
+$(‘html,body’).animate({scrollTop: targetOffset}, {duration:1600,easing:’easeInOutExpo’});
+
+return false;
+
+}
+
+}
+
+});
+
+});
